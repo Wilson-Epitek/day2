7ebd0dc2029454a488e3cf2c364fdc947b5e74fe
Wilson-Epitek <wilson.wu@epitech.digital>
2024-10-01 17:55:51 +0200

    Create ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,16 @@
+let a = 2
+let b = 4
+let action = "sub"
+
+function calculator() {
+  if (action === "add") return addition(a, b)
+  else if (action === "sub") {
+    return substraction(a, b)
+  }
+}
+function addition() {
+  return (b  = a + b)
+}
+function substraction() {
+  return (b = a - b)
+}
